c17947b6cc45f38f8ee136c8522837faeee981bd
P-SiZK <54477535+P-SiZK@users.noreply.github.com>
2019-11-27 17:38:41 +0900

    non-verified

---
+++ math/modint.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+//BEGIN
+template <typename T, T MOD = 1000000007>
+struct ModInt {
+	T x;
+	ModInt() :x(0) {}
+	ModInt(long long y) { x = y % MOD; if (x < 0) x += MOD; }
+
+	ModInt& operator+=(const ModInt& a) {
+		if ((x += a.x) >= MOD) x -= MOD;
+		return *this;
+	}
+	ModInt& operator-=(const ModInt& a) {
+		if ((x += MOD - a.x) >= MOD) x -= MOD;
+		return *this;
+	}
+	ModInt& operator*=(const ModInt& a) {
+		x = 1LL * x * a.x % MOD;
+		return *this;
+	}
+	ModInt& operator/=(const ModInt& a) {
+		*this *= a.inv();
+		return *this;
+	}
+
+	ModInt operator+(const ModInt& a) const { return ModInt(x) += a; }
+	ModInt operator-(const ModInt& a) const { return ModInt(x) -= a; }
+	ModInt operator*(const ModInt& a) const { return ModInt(x) *= a; }
+	ModInt operator/(const ModInt& a) const { return ModInt(x) /= a; }
+
+	ModInt operator-() const { return ModInt(-x); }
+
+	bool operator==(const ModInt a) const { return x == a.x; }
+	bool operator!=(const ModInt a) const { return x != a.x; }
+	bool operator<(const ModInt a) const { return x < a.x; }
+
+	friend ostream& operator<<(ostream& os, const ModInt<T>& a) {
+		return os << a.x;
+	}
+	friend istream& operator>>(istream& is, ModInt<T>& a) {
+		T t; is >> t;
+		a = ModInt(t);
+		return is;
+	}
+
+	ModInt inv() const {
+		T a = x, b = MOD, u = 1, v = 0;
+		while (b > 0) {
+			T t = a / b;
+			swap((a -= t * b), b);
+			swap((u -= t * v), v);
+		}
+		return ModInt(u);
+	}
+
+	ModInt pow(long long n) const {
+		ModInt res(1), tmp(x);
+		while (n > 0) {
+			if (n & 1) res *= tmp;
+			tmp *= tmp;
+			n >>= 1;
+		}
+		return res;
+	}
+
+	static ModInt comb(long long n, long long k) {
+		ModInt num(1), den(1);
+		for (int i = 0; i < k; ++i) {
+			num *= ModInt(n - i);
+			den *= ModInt(i + 1);
+		}
+		return num / den;
+	}
+};
+//END
+
+int main() {
+	
+
+	return 0;
+}
