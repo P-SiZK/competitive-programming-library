662d367b1ddf8ffa9cdc22ae098eae4aaa140439
P-SiZK <54477535+P-SiZK@users.noreply.github.com>
2020-04-06 17:42:16 +0900

    Create divisor.cpp

---
+++ math/divisor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+//BEGIN
+vector<long long> divisor(long long n) {
+    vector<long long> res;
+    for (long long i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i * i != n) res.push_back(n / i);
+        }
+    }
+    sort(res.begin(), res.end());
+    return res;
+}
+//END
+
+int main() {
+
+	return 0;
+}
