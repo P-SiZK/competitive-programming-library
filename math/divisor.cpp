be896f8d2f6c008d89ea47948c59a70f0ddacec2
P-SiZK <54477535+P-SiZK@users.noreply.github.com>
2020-04-12 22:59:43 +0900

    Update divisor.cpp

--- math/divisor.cpp
+++ math/divisor.cpp
@@ -7,7 +7,7 @@ using namespace std;
 //BEGIN
 vector<long long> divisor(long long n) {
     vector<long long> res;
-    for (long long i = 1; i * i <= n; i++) {
+    for (long long i = 1; i * i <= n; ++i) {
         if (n % i == 0) {
             res.push_back(i);
             if (i * i != n) res.push_back(n / i);
