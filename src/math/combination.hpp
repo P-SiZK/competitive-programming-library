f68266facc62add2171e3eb1bce2192d5d32d244
P-SiZK <54477535+P-SiZK@users.noreply.github.com>
2023-09-04 17:24:30 +0900

    add permutation

--- src/math/combination.hpp
+++ src/math/combination.hpp
@@ -1,26 +1,33 @@
 #ifndef MATH_COMBINATION_HPP
 #define MATH_COMBINATION_HPP
 
-#include <cstddef>
+#include <array>
 
-template<class T, std::size_t SIZE = 1100000, T MOD = 1000000007>
+template<class T, int SIZE = 1100000, T MOD = 1000000007>
 class Combination {
 private:
-	T fac[SIZE], finv[SIZE], inv[SIZE];
+	std::array<T, SIZE> fac, finv, inv;
 
 public:
 	Combination() {
 		fac[0] = fac[1] = inv[1] = finv[0] = finv[1] = 1;
-		for (std::size_t i = 2; i < SIZE; ++i) {
+		for (int i = 2; i < SIZE; ++i) {
 			fac[i] = fac[i - 1] * i % MOD;
 			inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
 			finv[i] = finv[i - 1] * inv[i] % MOD;
 		}
 	}
 
+	T inverse(int n) { return inv[n]; }
+
 	T factorial(int n) { return fac[n]; }
 
 	T inverse_factorial(int n) { return finv[n]; }
+
+	T permutation(int n, int r) {
+		if (n < r || n < 0 || r < 0) return 0;
+		return fac(n) * finv(n - r);
+	}
 
 	T combination(int n, int r) {
 		if (n < r || n < 0 || r < 0) return 0;
